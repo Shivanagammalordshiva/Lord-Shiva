614db3fb21c12e4147257b707fd171841d279a54
root <root@ip-172-31-32-230.ap-south-1.compute.internal>
2023-02-21 05:29:25 +0000

    Initial commit

---
+++ hello.sh
@@ -0,0 +1,7 @@
+echo "Hello World"
+echo "GM/GA/GE"
+date
+echo "Hello Everyone am new to the places."
+date
+touch /tmp/tempin.txt
+
---
+++ newone.java
@@ -0,0 +1,5 @@
+int a=10;
+int b=20;
+System.out.println(a);
+System.out.println(b);
+
