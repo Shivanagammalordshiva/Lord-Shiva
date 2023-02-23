cd00453288f5e2f5cf865a2170650e0d1e9686dc
= <=>
2023-02-23 03:29:06 +0000

    merge conflict fixed

--- hello.sh
+++ hello.sh
@@ -4,9 +4,5 @@ date
 echo "Hello Everyone am new to the places."
 touch /tmp/tempin.txt
 echo "Hello Telugu New year"
-<<<<<<< HEAD
-echo "Today date is 23rd Feb"
-=======
 echo "tejaswini"
->>>>>>> master
 
